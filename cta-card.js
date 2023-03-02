4b8f9906d912432c1a6aeb268da1e3ca4c96dfc2
Ragonesi Alessio <aleragonesi04@gmail.com>
2023-03-02 22:54:22 +0100

    added card as a JS component

--- cta-card.js
+++ cta-card.js
@@ -1,19 +1,30 @@
 {
-  const scriptTagList = document.getElementsByTagName("script");
-  const scriptTag = scriptTagList[scriptTagList.length - 1];
-  const parentTag = scriptTag.parentNode;
+  function getCurrentScriptAndParentTag() {
+    //https://stackoverflow.com/questions/10299846/obtain-a-reference-to-script-parent-element
+    
+    const scriptTagList = document.getElementsByTagName("script");
+    const scriptTag = scriptTagList[scriptTagList.length - 1];
+    const parentTag = scriptTag.parentNode;
+    return [scriptTag, parentTag];
+  }
+
+  const [scriptTag, parentTag] = getCurrentScriptAndParentTag();
+
   const newCard = document.createElement("div");
-  newCard.innerHTML = `<div class="cta-card__contents" color="${scriptTag.getAttribute("color")}" logo="${scriptTag.getAttribute("logo")}">
+  newCard.innerHTML = `<div class="cta-card__contents" color="${scriptTag.getAttribute(
+    "color"
+  )}" logo="${scriptTag.getAttribute("logo")}">
         <div class="cta-card__contents__image"></div>
-        <h3 class="cta-card__contents__title">iOS App</h3>
+        <h3 class="cta-card__contents__title">${scriptTag.getAttribute(
+          "title"
+        )}</h3>
         <p class="cta-card__contents__paragraph">
-          Create, browse and save palettes on the go.
+          ${scriptTag.getAttribute("description")}
         </p>
         <div class="cta-card__contents__cta">
-          <span>View on the App Store</span>
+          <span>${scriptTag.getAttribute("cta")}</span>
         </div>
       </div>`;
   newCard.classList.add("cta-card");
   parentTag.appendChild(newCard);
-  console.log(parentTag);
 }
