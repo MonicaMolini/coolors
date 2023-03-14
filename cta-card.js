559812dccf4eecd0cb19abd745e8b8d6079f9bd7
Ragonesi Alessio <aleragonesi04@gmail.com>
2023-03-14 21:23:45 +0100

    added card as a web component

--- cta-card.js
+++ cta-card.js
@@ -1,7 +1,7 @@
 {
   function getCurrentScriptAndParentTag() {
     //https://stackoverflow.com/questions/10299846/obtain-a-reference-to-script-parent-element
-    
+
     const scriptTagList = document.getElementsByTagName("script");
     const scriptTag = scriptTagList[scriptTagList.length - 1];
     const parentTag = scriptTag.parentNode;
@@ -14,7 +14,11 @@
   newCard.innerHTML = `<div class="cta-card__contents" color="${scriptTag.getAttribute(
     "color"
   )}" logo="${scriptTag.getAttribute("logo")}">
-        ${scriptTag.getAttribute("badge") === "true" ? '<div class="badge badge--pink _m-l-1">New</div>' : ""}
+        ${
+          scriptTag.getAttribute("badge") === "true"
+            ? '<div class="badge badge--pink _m-l-1">New</div>'
+            : ""
+        }
         <div class="cta-card__contents__image"></div>
         <h3 class="cta-card__contents__title">${scriptTag.getAttribute(
           "title"
@@ -29,3 +33,5 @@
   newCard.classList.add("cta-card");
   parentTag.appendChild(newCard);
 }
+
+
