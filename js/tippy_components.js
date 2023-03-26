c0db12377a602d0af860361895a76b713b837fa8
Raccoon6650 <mario.caruso098@gmail.com>
2023-03-27 00:07:29 +0200

    Tippy inserito da regolare

--- js/tippy_components.js
+++ js/tippy_components.js
@@ -2,11 +2,11 @@
 //import 'https://cdnjs.cloudflare.com/ajax/libs/tippy.js/2.5.4/tippy.css'
 
 function tippyCreator(className) {
-  console.log("Parto")
     tippy(document.querySelectorAll(`.${className}`), {
     content: function(reference) {
       return reference.getAttribute('data-tippy-content')
-    }
+    },
+    allowHTML: true
   });
 }
 tippyCreator('tippy');
